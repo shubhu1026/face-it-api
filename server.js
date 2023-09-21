c8817516dc9b993c047efa4322ec3413cd32d00a
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-21 01:10:24 -0400

    server.js changes for render.com

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const e = require('express');
-const express = require('express');
-const cors = require('cors');
-const knex = require('knex');
-const bcrypt = require('bcrypt-nodejs');
+require("dotenv").config();
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image')
+const e = require("express");
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
+const bcrypt = require("bcrypt-nodejs");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'face-it'
-    }
+  client: "pg",
+  connection: {
+    host: "dpg-ck5srlddrqvc73dvuct0-a",
+    port: 5432,
+    user: "face_it_db_user",
+    password: process.env.DB_PASSWORD,
+    database: "face_it_db",
+    ssl: "true",
+  },
 });
 
 const app = express();
@@ -25,13 +28,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('success');});
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
-app.put('/image', (req, res) => {image.handleImage(req, res, db)});
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+app.get("/", (req, res) => {
+  res.send("success");
+});
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(3000, () => {
-    console.log('app is running on port 3000');
+  console.log("app is running on port 3000");
 });
